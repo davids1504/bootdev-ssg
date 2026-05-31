28e7b933057bf58337448d32712e0f37965988ff
David <sambunjak.david@gmail.com>
2026-05-31 18:19:47 +0200

    Add b2b logic

--- src/blocks.py
+++ src/blocks.py
@@ -1,7 +1,46 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = list(map(lambda a: a.strip(), markdown.split("\n\n")))
     full_blocks = list(filter(is_not_empty, blocks))
     return full_blocks
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith("# "):
+        return BlockType.HEADING
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith(">") and is_not_empty(line):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith("- ") and is_not_empty(line):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        lines = block.split("\n")
+        for i in range(len(lines)):
+            if not lines[i].startswith(f"{i + 1}. "):
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
 
 
 def is_not_empty(x: str) -> bool:
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestBlocks(unittest.TestCase):
@@ -64,6 +64,62 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items\n- a\n- b\n- i\n- g\n- f\n- k\n- l\n- i\n- s\n- t",
             ],
         )
+
+    def test_b2b_heading(self):
+        block = "# Hello"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_b2b_code(self):
+        block = """```
+void main() {
+    print("Hello, Dart")
+    print("Assert me, Daddy")
+}
+```"""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_b2b_quote(self):
+        block = ">Hello\n> World"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_b2b_ul(self):
+        block = """- It's my world, and
+- You can't have it
+- It's my world, and
+- You can't have it"""
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    def test_b2b_ol(self):
+        block = """1. Take a wrench
+2. Build a car
+3. Drink beer
+4. Crash you wrench
+5. Drink a refreshing engine beverage"""
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+    def test_b2b_ul_false(self):
+        block = """- It's my world, and
+- You can't have it
+- It's my world, and
+-You can't have it"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_b2b_ol_false(self):
+        block = """1. Take a wrench
+2. Build a car
+3. Drink beer
+4. Crash you wrench
+6. Drink a refreshing engine beverage"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_b2b_code_false(self):
+        block = """```
+void main() {
+    print("Hello, Dart")
+    print("Assert me, Daddy")
+}
+``"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
