8ddf9969cab7cfedb70a650327faae89c0f7f075
David <sambunjak.david@gmail.com>
2026-08-09 22:19:16 +0200

    Add gh pages build support

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ import shutil
 from blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = ""
     template = ""
@@ -17,24 +17,24 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     html_page = template.replace("{{ Title }}", title).replace(
         "{{ Content }}", html_node
-    )
+    ).replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
     if os.path.dirname(dest_path) != "":
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as f:
         f.write(html_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dirs = os.listdir(dir_path_content)
     for item in dirs:
         full_path = os.path.join(dir_path_content, item)
         if os.path.isfile(full_path) and item.endswith('.md'):
-            generate_page(full_path, template_path, os.path.join(dest_dir_path, os.path.splitext(item)[0] + ".html"))
+            generate_page(full_path, template_path, os.path.join(dest_dir_path, os.path.splitext(item)[0] + ".html"), basepath)
         else:
             if not os.path.isfile(full_path):
                 if os.path.dirname(os.path.join(dest_dir_path, item)):
                     os.makedirs(os.path.join(dest_dir_path, item), exist_ok=True)
-                generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item))
+                generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item), basepath)
 
 
 def extract_title(markdown):
--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 import os
 import shutil
+import sys
 
 from generate_page import generate_pages_recursive
 
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
+    if len(sys.argv) > 0:
+        basepath = sys.argv[0]
+    else:
+        basepath = "/"
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
     if os.path.exists("static"):
-        copy_to_public("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+        copy_to_docs("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
-def copy_to_public(source, destination):
+def copy_to_docs(source, destination):
     dirs = os.listdir(source)
     for item in dirs:
         full_path = os.path.join(source, item)
@@ -21,7 +26,7 @@ def copy_to_public(source, destination):
             shutil.copy(full_path, destination)
         else:
             os.mkdir(os.path.join(destination, item))
-            copy_to_public(os.path.join(source, item), os.path.join(destination, item))
+            copy_to_docs(os.path.join(source, item), os.path.join(destination, item))
 
 
 main()
